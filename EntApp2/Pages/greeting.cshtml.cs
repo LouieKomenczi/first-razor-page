f5d8a95ac2013853fa399060d19671b9e669f158
louieKay <37403561+louieKay@users.noreply.github.com>
2019-01-23 19:59:19 +0000

    added month to the message

---
+++ EntApp2/Pages/greeting.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp2.Pages
+{
+    public class greetingModel : PageModel
+    {
+        public string Message { get; set; } = "this Hello is set in the model! ";
+
+
+        public void OnGet()
+        {
+            if(DateTime.Now.Hour >= 12)
+            {
+                Message += "Good Afternoon";
+            }
+            else
+            {
+                Message += "Good Morning";
+            }
+
+            Message += " - this is the month of " + DateTime.Now.ToString("MMMM");
+        }
+    }
+}
--- EntApp2/Startup.cs
+++ EntApp2/Startup.cs
@@ -15,11 +15,16 @@ namespace EntApp2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseMvc();
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
