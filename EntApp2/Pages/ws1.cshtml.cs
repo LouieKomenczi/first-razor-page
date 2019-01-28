375c9c9cc22536823ab78121584bb6fb26c24b88
louieKay <37403561+louieKay@users.noreply.github.com>
2019-01-28 19:38:05 +0000

    ws1 and ws1B done

---
+++ EntApp2/Pages/ws1.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp2.Pages
+{
+    public class ws1Model : PageModel
+    {
+        public string[] MyTestArray { get; set; } 
+        
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var name = Request.Form["name"];
+            Message = "Hello " + name;
+
+        }
+    }
+}
---
+++ EntApp2/Pages/ws1B.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EntApp2.Pages
+{
+    public class ws1BModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            var n1 = Convert.ToInt32(Request.Form["n1"]);
+            var n2 = Convert.ToInt32(Request.Form["n2"]);
+            var op = Request.Form["operation"];
+            if (op == "add")
+            {
+                Message = Convert.ToString(n1) + " + " + Convert.ToString(n2) + " = " + Convert.ToString(n1 + n2);
+            }
+            if (op == "sub")
+            {
+                Message = Convert.ToString(n1) + " - " + Convert.ToString(n2) + " = " + Convert.ToString(n1 - n2);
+            }
+            if (op == "mul")
+            {
+                Message = Convert.ToString(n1) + " * " + Convert.ToString(n2) + " = " + Convert.ToString(n1 * n2);
+            }
+            if (op == "div")
+            {
+                if(n2 == 0)
+                {
+                    Message = "Divison by 0 not possibe ";
+                }
+                else
+                {
+                    Message = Convert.ToString(n1) + " / " + Convert.ToString(n2) + " = " + Convert.ToString(n1 / n2);
+                }           
+                
+            }
+        }
+    }
+}
